c62b9100d4498a9b228605f5daf65f8e6ca6e358
Alexander Makarof <makarofalex@gmail.com>
2020-04-09 18:51:43 +0300

    added loader

--- client/frontend.js
+++ client/frontend.js
@@ -1,9 +1,19 @@
 import Vue from "https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.esm.browser.js";
 
+Vue.component("loader", {
+  template: `
+  <div style='display: flex; justify-content: center; align-items: center'>
+    <div class="spinner-border" role="status">
+      <span class="sr-only">Loading...</span>
+    </div>
+  </div>`,
+});
+
 new Vue({
   el: "#app",
   data() {
     return {
+      loading: false,
       form: {
         name: "",
         value: "",
@@ -33,8 +43,9 @@ new Vue({
     },
   },
   async mounted() {
-    const data = await request("/api/contacts");
-    console.log(data);
+    this.loading = true;
+    this.contacts = await request("/api/contacts");
+    this.loading = false;
   },
 });
 
